779bf28284440d4f6fbf79e273ac2b2814c2ecc1
Adam Smith <hagansmith@gmail.com>
2017-12-05 10:29:24 -0600

    display map using https://github.com/allenhwkim/angularjs-google-maps"

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,4 +2,4 @@
 
 "use strict";
 
-const app = angular.module("MeetHere", ["ngAnimate", "ngRoute", "ngSanitize"]);
+const app = angular.module("MeetHere", ["ngAnimate", "ngRoute", "ngSanitize", "ngMap"]);
