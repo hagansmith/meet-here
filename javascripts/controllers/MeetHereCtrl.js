14d4dcae9589ad7f8989e469478d568bec3b0942
Adam Smith <hagansmith@gmail.com>
2017-12-05 12:49:53 -0600

    work on alt maps load

--- javascripts/controllers/MeetHereCtrl.js
+++ javascripts/controllers/MeetHereCtrl.js
@@ -1,5 +1,20 @@
 "use strict";
 
-app.controller("MeetHereCtrl", function($scope){
+app.controller("MeetHereCtrl", function($scope, $window, MAP_CONFIG){
+
+  $window.GoogleMapsLoader.onLoad(function (google) {
+    console.log("maps loaded");
+      $window.GoogleMapsLoader.KEY = '${MAP_CONFIG}';
+      $window.GoogleMapsLoader.LIBRARIES = ['geometry', 'places'];
+      var options = {
+        zoom: 11,
+        center: new google.maps.LatLng(54.5767, -1.2355)
+      };
+      console.log(options);
+      new google.maps.Map(document.getElementById('map'), options);
+
+
+    // new google.maps.Map(document.getElementById('map'), mapOptions);
+  });
 
 });
