aaea57b50274113e2c897f9fd1300f5f65a12cce
Adam Smith <hagansmith@gmail.com>
2017-12-27 20:55:07 -0600

    update calls to address cors errors and implement node server

--- javascripts/services/MapService.js
+++ javascripts/services/MapService.js
@@ -1,19 +1,19 @@
 app.service("MapService", function ($http, $q, MAP_CONFIG, LocationService, MarkerService, MeetService){
 
   const getCoordByAddressQuery = (address) => {
-    return $http.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${MAP_CONFIG}`);
+    return $http.get(`https://safe-mesa-30633.herokuapp.com/api/geocode/?address=${address}&key=${MAP_CONFIG}`);
   };
 
   const reverseGeocode = (coord) => {
-    return $http.get (`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coord.lat},${coord.lng}&key=${MAP_CONFIG}`);
+    return $http.get (`https://safe-mesa-30633.herokuapp.com/api/geocode/?latlng=${coord.lat},${coord.lng}&key=${MAP_CONFIG}`);
   };
 
   const placeDetail = (placeID) => {
-    return $http.get (`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=${MAP_CONFIG}`);
+    return $http.get (`https://safe-mesa-30633.herokuapp.com/api/place/details/?placeid=${placeID}&key=${MAP_CONFIG}`);
   };
 
   const directions = (meet, middy) => {
-    return $http.get (`https://maps.googleapis.com/maps/api/directions/json?origin=${meet.marker1.address}&destination=${middy.lat},${middy.lng}&key=${MAP_CONFIG}`);
+    return $http.get (`https://safe-mesa-30633.herokuapp.com/api/directions/?origin=${meet.marker1.address}&destination=${middy.lat},${middy.lng}&key=${MAP_CONFIG}`);
   };
 
   return { directions, getCoordByAddressQuery, reverseGeocode, placeDetail};
